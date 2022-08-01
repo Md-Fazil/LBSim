e4140595c0173bd1f0f775541108904f3c59f027
Md-Fazil <fuzyll10598@gmail.com>
2022-08-01 20:23:56 +0800

    Add retry limit

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
 	"sync/atomic"
 	"time"
+)
+
+const (
+	Attempts int = iota
+	Retry
 )
 
 type Server struct {
@@ -98,3 +104,38 @@ func (s *ServerPool) HealthCheck() {
 		log.Printf("%s [%s]\n", b.URL, status)
 	}
 }
+
+// GetAttemptsFromContext returns the attempts for request
+func GetAttemptsFromContext(r *http.Request) int {
+	if attempts, ok := r.Context().Value(Attempts).(int); ok {
+		return attempts
+	}
+	return 1
+}
+
+// GetAttemptsFromContext returns the attempts for request
+func GetRetryFromContext(r *http.Request) int {
+	if retry, ok := r.Context().Value(Retry).(int); ok {
+		return retry
+	}
+	return 0
+}
+
+// lb load balances the incoming request
+func lb(w http.ResponseWriter, r *http.Request) {
+	attempts := GetAttemptsFromContext(r)
+	if attempts > 3 {
+		log.Printf("%s(%s) Max attempts reached, terminating\n", r.RemoteAddr, r.URL.Path)
+		http.Error(w, "Service not available", http.StatusServiceUnavailable)
+		return
+	}
+
+	peer := serverPool.GetNextServer()
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
+var serverPool ServerPool
