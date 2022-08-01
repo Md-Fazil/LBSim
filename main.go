9d7c54694ab21fcceba9dda1b537f97fff5adf4f
Md-Fazil <fuzyll10598@gmail.com>
2022-08-01 20:17:02 +0800

    Add round robin server selection

--- main.go
+++ main.go
@@ -29,18 +29,44 @@ func (b *Server) IsAlive() (alive bool) {
 	return
 }
 
-// ServerPool holds information about reachable backends
+// ServerPool holds information about reachable servers
 type ServerPool struct {
 	servers []*Server
 	current uint64
 }
 
 // AddBackend to the server pool
-func (s *ServerPool) AddBackend(backend *Server) {
+func (s *ServerPool) AddServer(backend *Server) {
 	s.servers = append(s.servers, backend)
 }
 
 // NextIndex atomically increase the counter and return an index
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.servers)))
+}
+
+// MarkBackendStatus changes a status of a server
+func (s *ServerPool) MarkServerStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.servers {
+		if b.URL.String() == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
+}
+
+// GetNextServer returns next active server to take a connection in round robin fashion
+func (s *ServerPool) GetNextServer() *Server {
+	next := s.NextIndex()
+	l := len(s.servers) + next
+	for i := next; i < l; i++ {
+		idx := i % len(s.servers)
+		if s.servers[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.servers[idx]
+		}
+	}
+	return nil
 }
